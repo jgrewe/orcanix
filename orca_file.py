28d0fd9b89d490869db1228f9dca25dc50e206b3
Jan Grewe <jan.grewe@g-node.org>
2014-12-15 00:06:32 +0100

    change to oo approach

--- orca_file.py
+++ orca_file.py
@@ -5,18 +5,20 @@ from __future__ import print_function, division
 import os
 import nix
 import time
-import numpy as np
 import uuid
 
 from IPython import embed
+import orca_general as og
+
 
 class OrcaFile(object):
     
     def __init__(self):
         self.nix_file = None
-        self.file_info = None
+        self.__section = None
+        self.__general_info = None
 
-    def open(self, filename, identifier=None, mode='read_only'):
+    def open(self, filename, mode='read_only'):
         if "read_only" in mode:
             file_mode = nix.FileMode.ReadOnly
         elif "read_write" in mode:
@@ -24,24 +26,39 @@ class OrcaFile(object):
         elif "overwrite" in mode:
             file_mode = nix.FileMode.Overwrite
         
-        if not os.path_exists(filename) or file_mode == nix.FileMode.Overwrite: 
-            self.nix_file = new(filename)
+        if not os.path.exists(filename) or file_mode == nix.FileMode.Overwrite:
+            self.new(filename)
         else:
             self.nix_file = nix.File.open(filename, file_mode)
         
     def new(self, filename, identifier=None, experiment_start_time=None):
         self.nix_file = nix.File.open(filename, nix.FileMode.Overwrite)
-        self.file_info = self.nix_file.create_section('orca file', 'orcanix.file')
-        self.file_info['orca_version'] = 0.2
-        self.orce_info['identifier'] = identifier if identifier else str(uuid.uuid4())
-        self.file_info['experiment_start_time'] = experiment_start_time if experiment_start_time else time.ctime() 
-        
-    
-    def file_info():
+        self.__section = self.nix_file.create_section('orca file', 'orca.file')
+        self.__section['orca_version'] = 0.2
+        self.__section['identifier'] = identifier if identifier else str(uuid.uuid4())
+        self.__section['experiment_start_time'] = experiment_start_time if experiment_start_time else time.ctime()
+
+    @property
+    def general_info(self):
+        return self.__general_info
+
+    @general_info.setter
+    def general_info(self, info):
+        if isinstance(info, og.OrcaGeneral.__class__):
+            self.__general_info = info
+        else:
+            raise Exception('Info parameter must be of type OrcaGeneral')
+
+    def file_info(self):
         return self.nix_file.sections['orca file']
 
 
 if __name__ == '__main__':
     of = OrcaFile()
     of.new('test.orca')
-    print (of.file_info())
+    general = og.OrcaGeneral(of.nix_file, 'session_1')
+    general.animal = 'animal_1'
+    general.animal.species = 'C.  Elegans'
+    general.device = 'Amplifier_1'
+    general.device.attributes = {'a' : 100, 'b' : 200}
+    embed()
