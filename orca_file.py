db553215f36c29451fb2e3ef089d81538235053b
Jan Grewe <jan.grewe@g-node.org>
2014-12-16 19:59:29 +0100

    add epochs to orca file

--- orca_file.py
+++ orca_file.py
@@ -9,15 +9,26 @@ import uuid
 import numpy as np
 from IPython import embed
 import orca_general as og
+import orca_epochs as oe
+
+"""
+FIXME:
+ Unclear whether there are more than one sessions in a single file?
+
+ What is the difference between the file identifier and the session id?
+
+ How are links between entities established?
+"""
 
 
 class OrcaFile(object):
     
     def __init__(self):
-        self.nix_file = None
+        self.__nix_file = None
         self.__section = None
         self.__block = None
         self.__general_info = None
+        self.__epochs = {}
 
     """
     TODO
@@ -38,9 +49,9 @@ class OrcaFile(object):
             self.__block =
     """
     def new(self, filename, identifier=None, experiment_start_time=None):
-        self.nix_file = nix.File.open(filename, nix.FileMode.Overwrite)
+        self.__nix_file = nix.File.open(filename, nix.FileMode.Overwrite)
         identifier = identifier if identifier else str(uuid.uuid4())
-        self.__section = self.nix_file.create_section('orca file', 'orca.file')
+        self.__section = self.__nix_file.create_section('orca file', 'orca.file')
         self.__section['orca_version'] = 0.2
         self.__section['identifier'] = identifier
         self.__section['experiment_start_time'] = experiment_start_time if experiment_start_time else time.ctime()
@@ -51,25 +62,27 @@ class OrcaFile(object):
         return self.__general_info
 
     @general_info.setter
-    def general_info(self, info):
-        if isinstance(info, og.OrcaGeneral.__class__):
-            self.__general_info = info
-        else:
-            raise Exception('Info parameter must be of type OrcaGeneral')
+    def general_info(self, session_id):
+        self.__general_info = og.OrcaGeneral(self.__nix_file, self.__block, session_id)
 
+    @property
     def file_info(self):
-        return self.nix_file.sections['orca file']
+        return self.__nix_file.sections['orca file']
 
+    def add_epoch(self, name, start_time, end_time=None):
+        if name in self.__epochs.keys():
+            raise KeyError('An epoch with this name already exists!')
+        self.__epochs[name] = oe.OrcaEpoch(self.__block, name, start_time, end_time)
 
 if __name__ == '__main__':
     of = OrcaFile()
-    of.new('test.orca')
-    general = og.OrcaGeneral(of.nix_file, 'session_1')
-    general.animal = 'animal_1'
-    general.animal.species = 'C.  Elegans'
-    general.animal.birthdate = '2014-12-10'
-    general.device = 'Amplifier_1'
-    general.device.attributes = {'a' : 100, 'b' : 200}
-    general.electrical = 'magic array'
-    general.electrical.electrode_map = np.arange(10, 0.5)
+    of.new('test.orca', 'test_session')
+    of.general_info = 'session_1'
+    of.general_info.animal = 'animal_1'
+    of.general_info.animal.species = 'C.  Elegans'
+    of.general_info.animal.birthdate = '2014-12-10'
+    of.general_info.device = 'Amplifier_1'
+    of.general_info.device.attributes = {'a' : 100, 'b' : 200}
+    of.general_info.electrical = 'magic array'
+    of.general_info.electrical.electrode_map = np.arange(10, 0.5)
     embed()
