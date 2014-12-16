11a9115a5933e5c2446555891422bf0010f4113d
Jan Grewe <jan.grewe@g-node.org>
2014-12-16 17:08:19 +0100

    first incomplete version of orca epoch

---
+++ orca_epochs.py
@@ -0,0 +1,53 @@
+# !/usr/bin/env python
+#  -*- coding: utf-8 -*-
+from __future__ import print_function, division
+import uuid
+import nix
+import numpy as np
+
+
+class OrcaEpoch(object):
+
+    def __init__(self, nix_block, name, start_time, end_time=None):
+        self.__block = nix_block
+        self.__tag = self.__block.create_tag(name, 'orca.epoch')
+        self.__ignore_interval_starts = None
+        self.__ignore_interval_ends = None
+        if end_time is not None:
+            self.__tag.extent = end_time - start_time
+
+    @property
+    def start_time(self):
+        return self.__tag.position
+
+    @property
+    def end_time(self):
+        return self.start_time + self.__tag.extent
+
+    @end_time.setter
+    def end_time(self, end_time):
+        self.__tag.extent = end_time - self.start_time
+
+    @property
+    def description(self):
+        return self.__tag.definition
+
+    @description.setter
+    def description(self, description):
+        self.__tag.definition = description
+
+    @property
+    def ignore_intervals(self):
+        if self.__ignore_intervals is None:
+            return None
+        else:
+            return zip(self.__ignore_interval_starts[:],
+                       self.__ignore_interval_ends[:] - self.__ignore_interval_starts[:])
+
+    @ignore_intervals.setter
+    def ignore_intervals(self, intervals):
+        if not isinstance(intervals, list):
+            raise TypeError('Intervals must be list of tuples!')
+        if len(intervals) is 0 or not isinstance(intervals[0], tuple):
+            raise TypeError('Intervals is empty or not a list of tuples!')
+        starts, ends = map(list, zip(*intervals))
