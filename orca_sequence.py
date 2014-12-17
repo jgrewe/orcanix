48a4d597f8f3a1edb7d9276b62722986971c2fe0
Jan Grewe <jan.grewe@g-node.org>
2014-12-17 08:55:03 +0100

    stub of orca sequence

---
+++ orca_sequence.py
@@ -0,0 +1,51 @@
+# !/usr/bin/env python
+#  -*- coding: utf-8 -*-
+from __future__ import print_function, division
+
+
+class OrcaSequence(object):
+
+    def __init__(self):
+        pass
+
+
+class OcraPostion(OrcaSequence):
+
+    def __init__(self):
+        pass
+
+
+class OrcaElectrical(OrcaSequence):
+
+    def __init__(self):
+        pass
+
+
+class OrcaPatchClamp(OrcaSequence):
+
+    def __init__(self):
+        pass
+
+
+class OrcaSpikeEvents(OrcaSequence):
+
+    def __init__(self):
+        pass
+
+
+class OrcaImage(OrcaSequence):
+
+    def __init__(self):
+        pass
+
+
+class OrcaOptical(OrcaImage):
+
+    def __init__(self):
+        pass
+
+
+class OrcaTwoPhoton(OrcaOptical):
+
+    def __init__(self):
+        pass
